9c69f9cc6de8a1c30da18cf93053e116900c232c
HulkChou <hulkchou@tw.ute.com>
2018-01-05 09:34:14 +0800

    視窗程式的簡單工廠，利用反射生產出視窗

---
+++ FormFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormLibrary
+{
+    public class FormFactory : IFormFactory
+    {
+        string _assName;
+        public FormFactory(string assName)
+        {
+            _assName = assName;
+        }
+        public virtual Form CreateForm(string formName)
+        {
+            string className = _assName + "." + formName;
+            return (Form)Assembly.Load(_assName).CreateInstance(className);
+        }
+    }
+}
